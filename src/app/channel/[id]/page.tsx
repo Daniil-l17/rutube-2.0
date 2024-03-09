288eeb29fdff7908f2ca6ed282b64b8b46c6aab6
Daniil-l17 <mlumed221@gmail.com>
2024-03-09 23:45:26 +0500

    fix-4

--- src/app/channel/[id]/page.tsx
+++ src/app/channel/[id]/page.tsx
@@ -1,23 +1,23 @@
+'use server'
 import { axiosBase } from '@/config/axiosConfig';
 import { ProvaderChannelUserProfile } from '@/provaders/ProvaderChannelUserProfile';
 import { Iuser } from '@/types/IUser';
 import { Metadata } from 'next';
 import { Fragment } from 'react';
 
-/*export const generateMetadata = async ({
+export const generateMetadata = async ({
   params,
 }: {
   params: { id: string };
 }): Promise<Metadata> => {
+  'use server'
   const { data } = await axiosBase.get<Iuser>(`/user/by-id/${params.id}`);
   return {
     title: `${data.name.trim() ? data.name : `Пользователь ${data.id}`}`,
   };
-};*/
-
-export const metadata: Metadata = {
-  title: 'Мой канал',
 };
+
+
 
 const page = ({ params }: { params: { id: string } }) => {
   return (
